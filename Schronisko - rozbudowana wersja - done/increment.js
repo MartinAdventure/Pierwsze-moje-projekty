e8d70cd4703ef61e1de69bbed1ffc68f516545f1
Martin <120313968+MartinAdventure@users.noreply.github.com>
2023-04-30 12:34:31 +0200

    increment.js

--- Schronisko - rozbudowana wersja - done/increment.js
+++ Schronisko - rozbudowana wersja - done/increment.js
@@ -56,6 +56,7 @@ for (i = 0; i < myNodelist.length; i++) {
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
+
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
@@ -66,22 +67,77 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
+function decrementAnimalCount(buttonClicked) {
+  input = buttonClicked.parentElement.children[2];
+  var inputValue = input.value;
+  var newValue = parseInt(inputValue) - 1;
+  input.value = newValue;
+
+  if(newValue >= 0){
+    input.value = newValue;
+}
+else{
+    input.value = 0; 
+    alert('Ilość nie może być mniejsza od zera!');
+}
+}
+
+function incrementAnimalCount(buttonClicked) {
+  input = buttonClicked.parentElement.children[2];
+  var inputValue = input.value;
+  var newValue = parseInt(inputValue) + 1;
+  input.value = newValue;
+}
+
+function closingButton(parentElement) {  
+  parentElement.style.display = "none";  
+  }
+
 function addElement() {
   var newPetName = document.querySelector('#newPet').value;
-  console.log(newPetName);
   var newPetContainer = document.createElement('div');
   newPetContainer.classList.add('box');
 
   var newPetLable = document.createElement('label');
   newPetLable.setAttribute('for', 'name'); 
-  newPetLable.innerText = newPetName
+  newPetLable.innerText = newPetName;
   newPetContainer.insertAdjacentElement('afterbegin', newPetLable); 
-  
-    
+
+  const decrementButton = document.querySelector('.newButtonDec');
+  var newButtonDec = document.createElement('div');
+  newButtonDec.classList.add('dec');
+  newButtonDec.innerText = "-";
+  newPetContainer.insertAdjacentElement('beforeend', newButtonDec);
+  newButtonDec.addEventListener('click', (event) => decrementAnimalCount(event.target)); 
+  console.log(newButtonDec);
+
+  const inputField = document.querySelector('.input-field');
+  var newInPutField = document.createElement('input'); 
+  newInPutField.setAttribute('value', '0');
+  newPetContainer.insertAdjacentElement('beforeend', newInPutField);
+  console.log(newInPutField);
+
+  const incrementButton = document.querySelector('.newButtonInc');
+  var newButtonInc = document.createElement('div');
+  newButtonInc.classList.add('inc');
+  newButtonInc.innerText = "+";
+  newPetContainer.insertAdjacentElement('beforeend', newButtonInc);
+  newButtonInc.addEventListener('click', (event) => incrementAnimalCount(event.target)); 
+  console.log(newButtonInc);
+
+  const closeButton = document.querySelector('box');
+  var newCloseButton = document.createElement("SPAN");
+  var newCloseButtonText = document.createTextNode("\u00D7");
+  newCloseButton.className = "close";
+  newCloseButton.appendChild(newCloseButtonText);
+  newPetContainer.appendChild(newCloseButton);
+  console.log(newCloseButton);
+
+  newCloseButton.addEventListener('click', (event) => {
+    closingButton(event.target.parentElement);
+  });
 
   document.querySelector('#myDIV').insertAdjacentElement('beforebegin', newPetContainer);
 }
 
 document.querySelector(".addBtn").addEventListener("click", addElement);
-
-
